fb0f43c103c938ace3b3564070f7869690893cb7
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-02 20:05:33 +0900

    Initial Commit

---
+++ prepare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prepare
+//
+//  Created by Takuya Ando on 2021/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
